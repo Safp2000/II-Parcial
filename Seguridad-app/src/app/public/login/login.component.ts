eb07a5af7ee10d4d9bfff703446de17a4e47ca5d
Sebastian Figueroa <sebasfig13@hotmail.com>
2020-09-20 16:39:14 -0600

    Subir servidores

---
+++ Seguridad-app/src/app/public/login/login.component.ts
@@ -0,0 +1,37 @@
+import { SecurityService } from './../../services/security.service';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user={
+    nombre: '',
+    password: ''
+  }
+
+  constructor(
+    private security: SecurityService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onLogin(){
+    this.security.login(this.user).subscribe(
+      (res) =>{
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['clientes']);
+
+      },(err)    => {
+        console.log(err);
+      }
+
+    );
+  }
+
+}
